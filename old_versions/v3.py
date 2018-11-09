ae007f77cd1837d4fd3de765d512d566f7e6e812
Artem Mezhenin <a.mezhenin@gmail.com>
2018-11-09 23:59:11 +0400

    v4

--- old_versions/v3.py
+++ old_versions/v3.py
@@ -2,6 +2,7 @@
 # Python 3.6
 
 # Import the Halite SDK, which will let you interact with the game.
+from collections import defaultdict
 import hlt
 
 # This library contains constant values.
@@ -16,33 +17,40 @@ from hlt.positionals import Direction
 import logging
 
 
-def attack_enemy(game, command_queue, all_ships):
+def attack_enemy(game, command_queue, ship_status):
     me = game.me
     game_map = game.game_map
 
-    # attack shipyard only if it is a duel
     enemies = game.players.values()
     enemies = list(filter(lambda x: x.id != me.id, enemies))
-    if len(enemies) != 1 or len(all_ships) == 0:
-        return all_ships
+    if len(enemies) != 1:
+        return
     enemy = enemies[0]
 
-    # find a closest ship and use to attack the enemy
-    closest_ship = all_ships[0]
-    min_distance = 9999
-    for ship in all_ships:
-        dist = game_map.calculate_distance(ship.position, enemy.shipyard.position)
-        logging.info("Ship %s distance %s" % (ship, dist))
-        if dist < min_distance:
-            logging.info("Selecting ship %s" % ship)
-            closest_ship = ship
-            min_distance = dist
+    # find attacking ship
+    logging.info("Statuses %s" % ship_status.items())
 
-    move = game_map.naive_navigate(closest_ship, enemy.shipyard.position)
-    command_queue.append(closest_ship.move(move))
-    all_ships = list(filter(lambda x: x.id != closest_ship.id, all_ships))
-    logging.info("Final list %s" % all_ships)
-    return all_ships
+    ship = list(filter(lambda x: x[1] == "attack_shipyard", ship_status.items()))
+
+    logging.info("Ships %s" % ship)
+    # if not found , assign one
+    if len(ship) == 0:
+        ship = list(me.get_ships())
+        if len(ship) == 0:
+            return
+        ship = ship[0]
+        logging.info("Turning %s ship into attacking ship" % ship)
+        ship_status[ship.id] = "attack_shipyard"
+        pass
+    # if found move it to the enemy shipyard
+    else:
+        ship = list(filter(lambda x: x.id == ship[0][0], me.get_ships()))[0]
+        logging.info("found ship %s" % repr(ship))
+
+    move = game_map.naive_navigate(ship, enemy.shipyard.position)
+    command_queue.append(ship.move(move))
+
+    pass
 
 
 """ <<<Game Begin>>> """
@@ -52,7 +60,7 @@ game = hlt.Game()
 # At this point "game" variable is populated with initial map data.
 # This is a good place to do computationally expensive start-up pre-processing.
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
-game.ready("amezhenin-v4")
+game.ready("amezhenin-v3")
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
 #   Here, you log here your id, which you can always fetch from the game object by using my_id.
@@ -70,6 +78,8 @@ def collect(ship):
     max_idx = halite_choices.index(max(halite_choices))
     return DIRECTIONS[max_idx]
 
+ship_status = defaultdict(lambda: "collecting")
+
 while True:
     # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
     #   running update_frame().
@@ -82,26 +92,39 @@ while True:
     #   end of the turn.
     command_queue = []
 
-    all_ships = me.get_ships()
-    all_ships = attack_enemy(game, command_queue, all_ships)
+    attack_enemy(game, command_queue, ship_status)
 
-    for ship in all_ships:
+    for ship in me.get_ships():
 
-        if ship.halite_amount >= constants.MAX_HALITE * 0.4:
-            # navigate to home
-            move = game_map.naive_navigate(ship, me.shipyard.position)
-            command_queue.append(ship.move(move))
+
+        if ship_status[ship.id] == "collecting":
+            if ship.halite_amount >= constants.MAX_HALITE * 0.4:
+                # navigate to home
+                ship_status[ship.id] = "returning"
+                move = game_map.naive_navigate(ship, me.shipyard.position)
+                command_queue.append(ship.move(move))
+            else:
+                move = collect(ship)
+                command_queue.append(ship.move(move))
+
+        elif ship_status[ship.id] == "returning":
+            if ship.halite_amount == 0:
+                ship_status[ship.id] = "collecting"
+            else:
+                move = game_map.naive_navigate(ship, me.shipyard.position)
+                command_queue.append(ship.move(move))
         else:
-            move = collect(ship)
-            command_queue.append(ship.move(move))
+            pass
 
     # If the game is in the first 200 turns and you have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    # game.turn_number <= 123
-    if len(all_ships) == 0 \
+    turns_to_generate = 3 if len(game.players) == 2 else 1
+    if game.turn_number <= turns_to_generate \
             and me.halite_amount >= constants.SHIP_COST \
             and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
+
+
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
